1b5abbb87ae12a2494900b62638dc15174af3d39
homeservicespecialistas-cmyk <homeservicespecialistas@gmail.com>
2025-11-12 22:38:21 -0500

    Refactor navigation and keyboard support for flipbook

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
 $(function() {
+  // Inicializa el flipbook
   var $bookBlock = $('#bb-bookblock');
-  var $navNext = $('#bb-nav-next');
-  var $navPrev = $('#bb-nav-prev');
-
   $bookBlock.bookblock({
     speed: 800,
     shadowSides: 0.8,
-    shadowFlip: 0.4
+    shadowFlip: 0.7
   });
 
-  $navNext.on('click', function() {
+  // Referencias a los botones
+  var $navNext = $('#bb-nav-next');
+  var $navPrev = $('#bb-nav-prev');
+
+  // Funciones para moverse entre páginas
+  function nextPage() {
     $bookBlock.bookblock('next');
-    return false;
+  }
+
+  function prevPage() {
+    $bookBlock.bookblock('prev');
+  }
+
+  // Eventos de botones
+  $navNext.off('click').on('click', function(e) {
+    e.preventDefault();
+    nextPage();
   });
 
-  $navPrev.on('click', function() {
-    $bookBlock.bookblock('prev');
-    return false;
+  $navPrev.off('click').on('click', function(e) {
+    e.preventDefault();
+    prevPage();
+  });
+
+  // Soporte con teclado
+  $(document).keydown(function(e) {
+    var keyCode = e.keyCode || e.which,
+        arrow = { left: 37, right: 39 };
+
+    switch (keyCode) {
+      case arrow.left:
+        prevPage();
+        break;
+      case arrow.right:
+        nextPage();
+        break;
+    }
   });
 });
