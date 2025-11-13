5b47e52f50281c1d6946582c1c239d312423b074
homeservicespecialistas-cmyk <homeservicespecialistas@gmail.com>
2025-11-13 02:01:05 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,17 @@
 $(function() {
+  var book = $('#book').bookblock({
+    speed: 900,
+    shadowSides: 0.6,
+    shadowFlip: 0.3
+  });
 
-  var Page = (function() {
+  $('#next').on('click', function() {
+    book.bookblock('next');
+    return false;
+  });
 
-    var config = {
-      $bookBlock: $('#bb-bookblock'),
-      $navNext: $('#bb-nav-next'),
-      $navPrev: $('#bb-nav-prev')
-    };
-
-    var init = function() {
-      config.$bookBlock.bookblock({
-        speed: 800,
-        shadowSides: 0.8,
-        shadowFlip: 0.4
-      });
-      initEvents();
-    };
-
-    var initEvents = function() {
-      var $slides = config.$bookBlock.children();
-
-      // Navegación con botones
-      config.$navNext.on('click touchstart', function() {
-        config.$bookBlock.bookblock('next');
-        return false;
-      });
-
-      config.$navPrev.on('click touchstart', function() {
-        config.$bookBlock.bookblock('prev');
-        return false;
-      });
-
-      // Navegación con teclado
-      $(document).keydown(function(e) {
-        var key = e.which;
-        if (key === 37) config.$bookBlock.bookblock('prev');
-        if (key === 39) config.$bookBlock.bookblock('next');
-      });
-    };
-
-    return { init: init };
-
-  })();
-
-  Page.init();
+  $('#prev').on('click', function() {
+    book.bookblock('prev');
+    return false;
+  });
 });
